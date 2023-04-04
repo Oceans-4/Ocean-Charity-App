1b166c252c9053608740df5f729ac57433fc1da1
Amer Kulovic <amer.kulovic@yahoo.com>
2023-04-04 11:08:19 -0500

    added button and styling to TextInfo component

--- src/components/HomeInfo.js
+++ src/components/HomeInfo.js
@@ -7,12 +7,12 @@ const HomeInfo = () => {
     <div>
       <section className="m-6">
         <div className="flex flex-wrap py-8 justify-center">
-          <TextInfo title="Helping the Ocean, one day at a time" text="Text goes here" image={water} />
-          <img style={{ height: "350px", width: "350px" }} src={water} />
+          <TextInfo title="We can make a difference, this won't be our first time." text="Our leadership team bring years of experience to bear on the greatest challenge of our time. We’re results driven, with a proven record of previous successes." image={water} buttonText="Learn More" />
+          <img style={{ height: "380px", width: "400px" }} src={water} />
         </div>
         <div className="flex flex-wrap py-8 justify-center">
-          <img style={{ height: "350px", width: "350px" }} src={beach} />
-          <TextInfo title="Helping the Ocean, one day at a time" text="Text goes here" image={water} />
+          <img style={{ height: "380px", width: "400px" }} src={beach} />
+          <TextInfo title="Helping the Ocean is a team effort. What can you do to help?" text="You can support Ocean Buddies and our cause in numerous ways, by donating, volunteering in our effort, or just purchasing items in our shop, you are helping change the ocean and our environment." image={water} buttonText="Make a Change" />
         </div>
       </section>
     </div>
--- src/components/TextInfo.js
+++ src/components/TextInfo.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 const TextInfo = (props) => {
-  const [isLeft, setIsLeft] = useState(false);
   return (
-    <div className="w-3/5 p-5 text-center py-4">
-      <h1 className="font-bold text-4xl text-[#0B2447]">{props.title}</h1>
-      <p className="mt-8">{props.text}</p>
+    <div className="w-3/5 p-5 text-center py-4 relative">
+      <h1 className="font-bold text-5xl text-[#0B2447]">{props.title}</h1>
+      <p className="mt-8 font-light">{props.text}</p>
+      <button className="bg-[#0B2447] text-white p-4 absolute bottom-4 left-36">{props.buttonText}</button>
     </div>
   );
 };
