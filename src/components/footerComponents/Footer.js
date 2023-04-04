7114487414c8f8201775aecc6cf115ba66b089ea
Amer Kulovic <amer.kulovic@yahoo.com>
2023-04-04 14:34:01 -0500

    created footer icons

--- src/components/footerComponents/Footer.js
+++ src/components/footerComponents/Footer.js
@@ -1,8 +1,48 @@
 import "./Footer.css";
+import { LogoFacebook, LogoInstagram, LogoLinkedin, LogoNodejs, LogoTwitter } from "react-ionicons";
 
 const Footer = () => {
   return (
     <footer>
+      <ul className="flex flex-row justify-center">
+        <li className="px-2">
+          <a>
+            <LogoFacebook color="white" height="35px" width="35px" />
+          </a>
+        </li>
+        <li className="px-2">
+          <a>
+            <LogoTwitter color="white" height="35px" width="35px" />
+          </a>
+        </li>
+        <li className="px-2">
+          <a>
+            <LogoInstagram color="white" height="35px" width="35px" />
+          </a>
+        </li>
+        <li className="px-2">
+          <a>
+            <LogoLinkedin color="white" height="35px" width="35px" />
+          </a>
+        </li>
+      </ul>
+      <ul className="text-white flex flex-row justify-center mt-6">
+        <li className="px-4">
+          <a>Home</a>
+        </li>
+        <li className="px-4">
+          <a>About</a>
+        </li>
+        <li className="px-4">
+          <a>Support</a>
+        </li>
+        <li className="px-4">
+          <a>Shop</a>
+        </li>
+        <li className="px-4">
+          <a>Contact</a>
+        </li>
+      </ul>
       <div className="waves">
         <div className="wave" id="wave1"></div>
         <div className="wave" id="wave2"></div>
--- src/components/homeComponents/Home.js
+++ src/components/homeComponents/Home.js
@@ -6,16 +6,15 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <div>
+      <div className="relative">
         <img src={homePagePhoto} />
         <div className="absolute bottom-14 right-2/4">
           <h1 className="text-white  p-4 text-4xl font-bold">Water pollution needs a fast solution.</h1>
           <Link to="/about">
-            <button className="bg-white text-[#19376D] bottom-14 left-5 mb-5 ml-5 p-4 text-2xl font-bold rounded-3xl">Learn More</button>
+            <button className="bg-white text-[#19376D] bottom-14 left-5 mb-5 ml-5 p-4 text-2xl font-bold">Learn More</button>
           </Link>
         </div>
       </div>
-
       <HomeMission />
       <HomeInfo />
     </>
