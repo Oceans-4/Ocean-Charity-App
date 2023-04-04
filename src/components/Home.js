771a791b3c3d0f375491da14392a98c2184580ae
Amer Kulovic <amer.kulovic@yahoo.com>
2023-04-03 22:19:27 -0500

    imported tailwinds cli

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+const Home = () => {
+  return (
+    <>
+      <Nav />
+      <div>Homepage</div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+const Nav = () => {
+  return (
+    <ul className="bg-blue-500">
+      <li>Home</li>
+      <li>About</li>
+      <li>Support</li>
+      <li>Contact</li>
+      <li>Shop</li>
+    </ul>
+  );
+};
+
+export default Nav;
