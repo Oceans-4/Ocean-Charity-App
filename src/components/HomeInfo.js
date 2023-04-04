8690211269dda458a445d92e8fcc0eba191092ff
Amer Kulovic <amer.kulovic@yahoo.com>
2023-04-04 00:11:32 -0500

    imporved styling od HomeInfo

--- src/components/HomeInfo.js
+++ src/components/HomeInfo.js
@@ -1,8 +1,12 @@
 const HomeInfo = () => {
   return (
-    <section className="bg-[#576CBC] p-10 flex flex-wrap">
-      <h1 className="text-white text-2xl font-bold w-1/2 border-red-50 border">Go green; breathe clean. Wipe out pollution before it wipes you out. Keep the oceans blue, the planet green and the animals safe. We have got only one Earth.</h1>
-      <h1 className="text-white text-2xl font-bold w-1/2 border-red-50 border">Let's make a change!</h1>
+    <section className="bg-[#576CBC] p-10 flex flex-wrap items-center">
+      <div className="w-1/2 text-center">
+        <h1 className="text-white text-2xl font-bold ">Go green; breathe clean. Wipe out pollution before it wipes you out. Keep the oceans blue, the planet green and the animals safe. We have got only one Earth.</h1>
+      </div>
+      <div className="w-1/2 text-center">
+        <h1 className="text-white text-2xl font-bold ">Let's make a change!</h1>
+      </div>
     </section>
   );
 };
