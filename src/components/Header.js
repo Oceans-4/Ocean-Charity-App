9bba4835d06200ea483f56b17d44ebd016467ca1
Amer Kulovic <amer.kulovic@yahoo.com>
2023-04-04 00:21:53 -0500

    improved header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,10 @@ const Header = () => {
     <div className={classes.header}>
       <h1 className="text-4xl px-5 pt-7 text-white font-bold">Ocean Buddies</h1>
       <Nav />
-      <h1 className="text-white absolute bottom-1/4 right-2/4 mb-14 ml-5 p-4 text-4xl font-bold">Water pollution needs a fast solution.</h1>
-      <button className="bg-white text-[#19376D] absolute bottom-14 left-5 mb-5 ml-5 p-4 text-2xl font-bold">Learn More</button>
+      <div className="absolute bottom-14 right-2/4">
+        <h1 className="text-white  p-4 text-4xl font-bold">Water pollution needs a fast solution.</h1>
+        <button className="bg-white text-[#19376D] bottom-14 left-5 mb-5 ml-5 p-4 text-2xl font-bold rounded-3xl">Learn More</button>
+      </div>
     </div>
   );
 };
